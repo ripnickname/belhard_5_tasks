df81bcb952edbad2496a8e6fe090a7b2b0b91ef0
Pavel Buividovich <pbuividovich@aristeksystems.com>
2021-09-01 19:07:01 +0300

    pbuividovich_hw5

--- tasks/hard/selection_sort.py
+++ tasks/hard/selection_sort.py
@@ -19,6 +19,12 @@
 
 
 def selection_sort(array: list) -> list:
+    for i in range(len(array)):
+        min_value = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[min_value]:
+                min_value = j
+        array[i], array[min_value] = array[min_value], array[i]
     return array
 
 
